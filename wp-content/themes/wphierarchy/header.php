aa4a8b7a80d10b7d53084d35e936848e7df1f491
fullstackdev710 <fullstack710dev@gmail.com>
2023-04-23 23:01:04 -0400

    DEMO-WordPress Filter Hook - apply_filters

--- wp-content/themes/wphierarchy/header.php
+++ wp-content/themes/wphierarchy/header.php
@@ -1,17 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?>>
 
-<head>
-   <meta charset="UTF-8">
-   <?php wp_head(); ?>
-</head>
+    <div id="page">
 
-<body <?php body_class(); ?>>
-   <nav id="site-navigation" class="main-navigation" role="navigation">
-      <?php
-      $args = [
-         'theme_location' => 'main-menu'
-      ];
-      wp_nav_menu($args);
-      ?>
-   </nav>
+      <a href="#content" class="skip-link screen-reader-text">
+        <?php esc_html_e( 'Skip to content', 'wphooks' ); ?>
+      </a>
+
+      <header id="masthead" class="site-header" role="banner">
+
+        <div class="site-branding">
+          <p class="site-title">
+            <a href="<?php echo esc_url( home_url() ) ;?>" rel="home">
+              <?php bloginfo( 'name' ); ?>
+            </a>
+          </p>
+          <p class="site-description" >
+            <?php bloginfo( 'description' ); ?>
+          </p>
+        </div>
+
+        <nav id="site-navigation" class="main-navigation" role="navigation">
+          <?php
+            $args = [
+              // Location pickable in Customizer
+              'theme_location'  =>  'main-menu',
+              // Assigns a default menu to location
+              'menu'            =>  'Main Menu',
+            ];
+            wp_nav_menu( $args );
+          ?>
+        </nav>
+
+      </header>
+
+      <div id="content" class="site-content">
