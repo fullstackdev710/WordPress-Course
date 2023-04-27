99a83b609b2d06adf5a31f175d2cc4ba59403147
fullstackdev710 <fullstack710dev@gmail.com>
2023-04-27 14:04:51 -0400

    Setting Sub Pages

--- wp-content/plugins/6.02.01-admin-footer-credits-simple/6.02.01-admin-footer-credits-simple.php
+++ wp-content/plugins/6.02.01-admin-footer-credits-simple/6.02.01-admin-footer-credits-simple.php
@@ -4,23 +4,84 @@ Plugin Name: 6.02.01 - Simple Plugin
 Description: Displays a message in the admin footer.
 Version: 1.0.0
 Contributors: zgordon
-Author: Zac Gordon
-Author URI: https://zacgordon.com
+Author: Peace Marketing
+Author URI: https://peace-marketing.com/
 License: GPLv2 or later
 Text Domain: wpplugin
 */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (!defined('WPINC')) {
+  die;
 }
 
-function wpplugin_custom_admin_footer( $footer ) {
+function wpplugin_settings_pages()
+{
+  add_menu_page(
+    __('Plugin Name', 'wpplugin'),
+    __('Plugin Menu', 'wpplugin'),
+    'manage_options',
+    'wpplugin',
+    'wpplugin_settings_page_markup',
+    'dashicons-wordpress-alt',
+    100
+  );
 
-  $new_footer = str_replace( '.</span>', __(' and <a href="https://zacgordon.com">Zac Gordon</a>.</span>', 'wpplugin' ), $footer);
-  return $new_footer;
+  add_submenu_page(
+    'wpplugin',
+    __('Plugin Feature 1', 'wpplugin'),
+    __('Feature 1', 'wpplugin'),
+    'manage_options',
+    'wpplugin-feature-1',
+    'wpplugin_settings_subpage_markup'
+  );
 
+  add_submenu_page(
+    'wpplugin',
+    __('Plugin Feature 2', 'wpplugin'),
+    __('Feature 2', 'wpplugin'),
+    'manage_options',
+    'wpplugin-feature-2',
+    'wpplugin_settings_subpage_markup'
+  );
 }
-add_filter( 'admin_footer_text', 'wpplugin_custom_admin_footer', 10, 1 );
+add_action('admin_menu', 'wpplugin_settings_pages');
 
+function wpplugin_settings_page_markup()
+{
+  if (!current_user_can('manage_options')) {
+    return;
+  }
 ?>
+  <div class="wrap">
+    <h1><?php esc_html_e(get_admin_page_title()); ?></h1>
+    <p><?php esc_html_e('Some content.'); ?></p>
+  </div>
+<?php
+}
+
+function wpplugin_settings_subpage_markup()
+{
+  if (!current_user_can('manage_options')) {
+    return;
+  }
+?>
+  <div class="wrap">
+    <h1><?php esc_html_e(get_admin_page_title()); ?></h1>
+    <p><?php esc_html_e('Some content.', 'wpplugin'); ?></p>
+  </div>
+<?php
+}
+
+function wpplugin_default_sub_pages()
+{
+  add_dashboard_page(
+    __('Cool Default Sub Page', 'wpplugin'),
+    __('Custom Sub Page', 'wpplugin'),
+    'manage_options',
+    'wpplugin-subpage',
+    'wpplugin_settings_page_markup'
+  );
+}
+
+add_action('admin_menu', 'wpplugin_default_sub_pages');
--- wp-content/plugins/6.02.01-admin-footer-credits-simple/includes/admin-footer-text.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function wpplugin_custom_admin_footer($footer)
-{
-   $new_footer = str_replace('.</span>', __(' and <a href="https://peace-marketing.com/"Peace Marketing</a>.</span>', 'wpplugin'), $footer);
-   return $new_footer;
-}
-
-add_filter('admin_footer_text', 'wpplugin_custom_admin_footer', 10, 1);
